32c0a0c18c81bc4453f1784d22329afdc06803df
Adedamola Yusuff <adedamolayusuff341@gmail.com>
2026-02-23 23:02:49 +0200

    Added tic and toc

--- EEE4120F-master/Practical-1/run_analysis.m
+++ EEE4120F-master/Practical-1/run_analysis.m
@@ -16,61 +16,50 @@
 % REQUIREMENT: You may NOT use built-in convolution functions (conv2, imfilter, etc.)
 
 % TODO:
-%***************************************************************************************
-% padding the image with zeroes before processing
-% TODO: 
-% a.	padding the image with zeroes before processing
-
-%============================================================
-%function padded_image = ZeroPad(image, pad)
-%	[rows,cols] = size (image)	% extracting the dimensions of the image
-%	padded_image = zeros(rows+2*pad, cols+2*pad) % create large frame filled with zeroes
-%	padded_image(pad+1:rows+pad,pad+1:pad+cols) = image	% insert image into frame padded with zeroes
-%end
-%****************************************************************************************
-
 % Implement manual 2D convolution using Sobel Operator(Gx and Gy)
 % output - Convolved image result (grayscale)
+% Call the images into the code as arrays
 %========================================================================
 
 % Gx and Gy are for testing purposes
-Gx = [-1 0 1; -2 0 2; -1 0 1]
-Gy = [1 2 1; 0 0 0; -1 -2 -1]
+Gx = [-1 0 1; -2 0 2; -1 0 1];
+Gy = [1 2 1; 0 0 0; -1 -2 -1];
 
 % An image for testing purposes
-test_image = [2 5 3 6 3; 5 7 2 6 8; 1 2 3 4 5; 6 7 8 9 1; 2 3 4 5 6; 7 8 9 1 2; 3 4 5 6 7; 8 9 1 2 3]
+test_image = [2 5 3 6 3; 5 7 2 6 8; 1 2 3 4 5; 6 7 8 9 1; 2 3 4 5 6; 7 8 9 1 2; 3 4 5 6 7; 8 9 1 2 3];
 
 % Convolution Function
-function processed_image = my_conv2(image, Gx, Gy)
-
+function [processed_image, execution_time] = my_conv2(image, Gx, Gy)
+    tStart = tic;
     % No padding on processed image so processed image is smaller than original by 1 pixel "frame"
-    processed_image = zeros(size(image)-2)
+    processed_image = zeros(size(image)-2);
     
     % Get the dimensions of the processed image (we know this before we start convolving
     % depending on the type of padding that we choose beforehand)
-    num_rows = height(processed_image)
-    num_columns = width(processed_image)
+    num_rows = height(processed_image);
+    num_columns = width(processed_image);
     
     % Convolution of both Gx and Gy as well as summation of magnitudes is
     % done in this for-loop
     
-    for r = 1:num_rows                  % Pixel row position on processed image
-        for c = 1:num_columns           % Pixel column position of processed image
-            image_pixel_row = r+1       % "Same pixel" but its row position on the original image
-            image_pixel_column = c+1    % "Same pixel" but its column position on the original image
+    for r = 1:num_rows                   % Pixel row position on processed image
+        for c = 1:num_columns            % Pixel column position of processed image
+            image_pixel_row = r+1;       % "Same pixel" but its row position on the original image
+            image_pixel_column = c+1;    % "Same pixel" but its column position on the original image
             
-            totalGx=0
-            totalGy=0
+            totalGx=0;
+            totalGy=0;
 
             for rG = 1:3                % Position on Gx and Gy
                 for cG = 1:3            % Position on Gx and Gy
-                    totalGx = Gx(rG,cG)*image(image_pixel_row-2+rG,image_pixel_column-2+cG) + totalGx  % Gives the horizontal gradient at the current pixel. The math here allows us to go to the adjacent squares while sequentially moving through Gx 
-                    totalGy = Gy(rG,cG)*image(image_pixel_row-2+rG,image_pixel_column-2+cG) + totalGy  % Gives the vertical gradient at the current pixel. The math here allows us to go to the adjacent squares while sequentially moving through Gy
+                    totalGx = Gx(rG,cG)*image(image_pixel_row-2+rG,image_pixel_column-2+cG) + totalGx;  % Gives the horizontal gradient at the current pixel. The math here allows us to go to the adjacent squares while sequentially moving through Gx 
+                    totalGy = Gy(rG,cG)*image(image_pixel_row-2+rG,image_pixel_column-2+cG) + totalGy;  % Gives the vertical gradient at the current pixel. The math here allows us to go to the adjacent squares while sequentially moving through Gy
                 end
             end
-            processed_image(r,c) = abs(totalGx) + abs(totalGy)
+            processed_image(r,c) = abs(totalGx) + abs(totalGy);
         end
     end
+    execution_time = toc(tStart);
 end
 
 %% ========================================================================
@@ -81,16 +70,15 @@ end
 
 % TODO: Use conv2 to perform 2D convolution
 % output - Convolved image result (grayscale)
-function output = inbuilt_conv2(image, Gx, Gy) % pass image and operators
-% cast image to double to avoid overflow
-
-% applying the conv2 for the sobel kernels
+function [output_matrix, execution_time] = inbuilt_conv2(image, Gx, Gy) % pass image and operators
+    tStart = tic;
+    % applying the conv2 for the sobel kernels
 	Gx_out = conv2(image, Gx, "valid");
 	Gy_out = conv2(image, Gy, "valid");
 
-% clculating magnitude
-	output =  abs(Gx_out) + abs(Gy_out)
-
+    % calculating magnitude
+	output_matrix =  abs(Gx_out) + abs(Gy_out);
+    execution_time = toc(tStart);
 end
 
 %% ========================================================================
@@ -101,6 +89,19 @@ end
 % built-in conv2 function (inbuilt_conv2).
 
 function run_analysis()
+
+    images = dir('sample_images/*.png');
+
+    for i = 1:length(images)
+    
+        filename = fullfile(images(i).folder, images(i).name);
+        img = imread(filename);
+    
+        img = rgb2gray(img);
+    
+        result = my_conv2(img, Gx, Gy);
+    
+    end
     % TODO1:
     % Load all the sample images from the 'sample_images' folder
     
